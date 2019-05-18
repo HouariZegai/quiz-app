12b99f78bac289d169d1f376079b86569ea6e5b4
unknown <houarizegai14@gmail.com>
2019-05-18 16:43:53 +0200

    Making main class and run first test

---
+++ src/main/java/com/houarizegai/quizappfx/App.java
@@ -0,0 +1,24 @@
+package com.houarizegai.quizappfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class App extends Application {
+
+    public static Stage stage = null;
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/JavaQuiz.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Java Quiz");
+        App.stage = stage;
+        stage.show();
+    }
+}
